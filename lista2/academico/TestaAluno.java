84705a902a422b453368f2a37a0c52b4cc6f07b4
Daniel Pires <dfpires@gmail.com>
2021-08-20 22:00:43 -0300

    20-08

--- lista2/academico/TestaAluno.java
+++ lista2/academico/TestaAluno.java
@@ -7,5 +7,12 @@ public class TestaAluno {
         // JOptionPane.showInputDialog("Informe sua idade") retorna uma String
         // Integer.parseInt converte String em int
         obj1.setIdade(Integer.parseInt(JOptionPane.showInputDialog("Informe sua idade")));
+        obj1.setNumeroAluno(Integer.parseInt(JOptionPane.showInputDialog("Informe número do aluno")));
+        obj1.setNome(JOptionPane.showInputDialog("Informe seu nome"));
+         // Float.parseFloat converte String em float
+        obj1.setP1(Float.parseFloat(JOptionPane.showInputDialog("Informe nota 1")));
+        obj1.setP2(Float.parseFloat(JOptionPane.showInputDialog("Informe nota 2")));
+        // null indica que a caixa de diálogo não tem pai
+        JOptionPane.showMessageDialog(null, obj1.toString());
     }
 }
