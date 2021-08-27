14713b50d44f1ed663ffcc58411760f0a794f482
Daniel Pires <dfpires@gmail.com>
2021-08-27 20:36:52 -0300

    27-08

--- lista3/viagem/Reserva.java
+++ lista3/viagem/Reserva.java
@@ -1,5 +1,68 @@
 package lista3.viagem;
+import java.util.Date;
 
 public class Reserva {
+    private int codigo;
+    private Date data;
+    private float valor;
+    // associação
+    private Passageiro passageiro; // Passageiro é a classe e passageiro é o objeto
+    private Voo voo; // Voo é a classe e voo é o objeto
+
+    public Reserva() {
+    }
+    public Reserva(int codigo, Date data, float valor, Passageiro passageiro, Voo voo) {
+        this.setCodigo(codigo);
+        this.setData(data);
+        this.setValor(valor);
+        this.setPassageiro(passageiro);
+        this.setVoo(voo);
+    }
+    public Voo getVoo() {
+        return voo;
+    }
+
+    public void setVoo(Voo voo) {
+        this.voo = voo;
+    }
+
+    public Passageiro getPassageiro() {
+        return passageiro;
+    }
+
+    public void setPassageiro(Passageiro passageiro) {
+        this.passageiro = passageiro;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+   public String toString(){
+       return "Código: " + this.codigo + " data: " + this.data + " Valor: " + this.valor + 
+       " \n Passageiro: " + this.passageiro.toString() + "\n Voo: " + this.voo.toString();
+   }
+   
     
+
 }
--- lista3/viagem/Voo.java
+++ lista3/viagem/Voo.java
@@ -1,5 +1,45 @@
 package lista3.viagem;
+import java.util.Date;
 
 public class Voo {
-    
+    private int numero;
+    private String origem, destino;
+    private Date data;
+
+    public Voo() {
+    }
+    public Voo(int numero, String origem, String destino, Date data) {
+        this.setNumero(numero);
+        this.setOrigem(origem);
+        this.setDestino(destino);
+        this.setData(data);
+    }
+    public Date getData() {
+        return data;
+    }
+    public void setData(Date data) {
+        this.data = data;
+    }
+    public String getDestino() {
+        return destino;
+    }
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    public String getOrigem() {
+        return origem;
+    }
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String toString(){
+        return "Número: " + this.numero + " Origem: " + this.origem + " Destino " + this.destino + " Data: " + this.data;
+    } 
+
 }
