60051c12031c4b2db750c837c73df428bf0134d2
Daniel Pires <dfpires@gmail.com>
2021-08-27 21:54:48 -0300

    27-08

--- lista3/ecommerce/Carrinho.java
+++ lista3/ecommerce/Carrinho.java
@@ -1,5 +1,63 @@
 package lista3.ecommerce;
+import java.util.Date;
 
 public class Carrinho {
+    private int codigo;
+    private float valorTotal;
+    private String formaPgto;
+    private Date data;
+    private String formaEntrega;
+    // associação
+    private ItemCarrinho itemCarrinho;
+
+    public Carrinho() {
+    }
     
+    public Carrinho(int codigo, float valorTotal, String formaPgto, Date data, String formaEntrega,
+    int codigoIC, float valorIC, int qtdeIC) {
+        this.setCodigo(codigo);
+        this.setValorTotal(valorTotal);
+        this.setFormaPgto(formaPgto);
+        this.setData(data);
+        this.setFormaEntrega(formaEntrega);
+        this.itemCarrinho = new ItemCarrinho(codigoIC, valorIC, qtdeIC); // composição
+    }
+    public ItemCarrinho getItemCarrinho() {
+        return itemCarrinho;
+    }
+    public void setItemCarrinho(int codigoIC, float valorIC, int qtdeIC) {
+        this.itemCarrinho = new ItemCarrinho(codigoIC, valorIC, qtdeIC); // composição
+    }
+    public String getFormaEntrega() {
+        return formaEntrega;
+    }
+    public void setFormaEntrega(String formaEntrega) {
+        this.formaEntrega = formaEntrega;
+    }
+    public Date getData() {
+        return data;
+    }
+    public void setData(Date data) {
+        this.data = data;
+    }
+    public String getFormaPgto() {
+        return formaPgto;
+    }
+    public void setFormaPgto(String formaPgto) {
+        this.formaPgto = formaPgto;
+    }
+    public float getValorTotal() {
+        return valorTotal;
+    }
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+   
+
 }
--- lista3/ecommerce/ItemCarrinho.java
+++ lista3/ecommerce/ItemCarrinho.java
@@ -2,4 +2,38 @@ package lista3.ecommerce;
 
 public class ItemCarrinho {
     
+    private int codigo;
+    private float valor;
+    private int qtde;
+    public ItemCarrinho() {
+    }
+    public ItemCarrinho(int codigo, float valor, int qtde) {
+        this.setCodigo(codigo);
+        this.setValor(valor);
+        this.setQtde(qtde);
+    }
+    public int getQtde() {
+        return qtde;
+    }
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+    public float getValor() {
+        return valor;
+    }
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+   
+  public String toString(){
+      return "Código: " + this.codigo + "Valor: " + this.valor + " Qtde " + this.qtde;
+  }
+
+    
 }
