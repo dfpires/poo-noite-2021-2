3feefc05ac15c71cc282ab6ce59babe6b30dee2e
Daniel Pires <dfpires@gmail.com>
2021-08-27 21:22:08 -0300

    27-08

--- lista3/viagem/Testa.java
+++ lista3/viagem/Testa.java
@@ -1,9 +1,16 @@
 package lista3.viagem;
 
+import java.util.Date;
+
 public class Testa {
     
     public static void main(String args[]){
-        Passageiro pas1 = new Passageiro();
+        Passageiro pas1 = new Passageiro("23.646.356-1", "196.318.968-03", "Pedro");
+
+        Voo vo1 = new Voo(123, "Congonhas", "Galeão", new Date());
        
+        Reserva re1 = new Reserva(3, new Date(), 3600, pas1, vo1);
+
+        System.out.println(re1.toString());
     }
 }
