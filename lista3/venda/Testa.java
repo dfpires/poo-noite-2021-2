faa8a4551db902d8d9ef6f1a5738e505706e1729
Daniel Pires <dfpires@gmail.com>
2021-09-02 21:58:38 -0300

    02-09

--- lista3/venda/Testa.java
+++ lista3/venda/Testa.java
@@ -1,11 +1,15 @@
 package lista3.venda;
-
+import java.util.Date;
 public class Testa {
     
-    public static void main(){
+    public static void main(String args[]){
 
-        Venda v1 = new Venda();
+        Venda v1 = new Venda(10, 500, new Date());
+        v1.addItemVenda(1, "Alcool em gel", 5, 10);
+        v1.addItemVenda(2, "Máscara", 3, 10);
+        v1.addItemVenda(3, "Camiseta", 10, 42);
 
-        
+        System.out.println(v1.toString());
+
     }
 }
